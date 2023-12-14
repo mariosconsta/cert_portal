cae7137846eda0b628555ed1eb4c954a5ca91a5f
Marios Constantinou <mariosconsta@gmail.com>
2023-12-14 20:41:42 +0200

    Validation for each field done

--- src/App.jsx
+++ src/App.jsx
@@ -15,28 +15,42 @@ const App = () => {
       name: "addressTo",
       type: "text",
       placeholder: "Required",
+      errorMessage:
+        "This field is required and only alphanumeric values are allowed.",
       label: "Address To",
+      pattern: "^[A-Za-z0-9]+$",
+      required: true,
     },
     {
       id: 2,
       name: "employeeId",
       type: "text",
       placeholder: "Required",
+      errorMessage:
+        "This field is required and only numeric values are allowed.",
       label: "Employee Id",
+      pattern: "^[0-9]+$",
+      required: true,
     },
     {
       id: 3,
       name: "issuedOn",
       type: "date",
       placeholder: "Required",
+      errorMessage: "This field is required and only future dates are allowed",
       label: "Issued On",
+      required: true,
     },
     {
       id: 4,
       name: "purpose",
-      type: "text",
+      type: "textarea",
       placeholder: "Required",
+      errorMessage:
+        "This field is required and the input must be more than 50 characters.",
       label: "Purpose",
+      pattern: "^.{50,}$",
+      required: true,
     },
   ];
 
@@ -50,9 +64,11 @@ const App = () => {
   };
 
   console.log(values);
+
   return (
     <div className="app">
       <form onSubmit={handleSubmit}>
+        <h1>Certification request platform</h1>
         {inputs.map((input) => (
           <FormInput
             key={input.id}
@@ -61,7 +77,10 @@ const App = () => {
             onChange={onChange}
           />
         ))}
-        <button>Click me</button>
+        <div className="formButtons">
+          <button className="submitButton">Submit Request</button>
+          <button className="requestButton">Submitted Requests</button>
+        </div>
       </form>
     </div>
   );
